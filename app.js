3b4df0803b4a38c7974852c8aa749cea240936f9
alkan589 <84925672+alkan589@users.noreply.github.com>
2023-03-26 17:42:24 +0300

    Add files via upload
    my description

---
+++ app.js
@@ -0,0 +1,7 @@
+// create an empty object using the Object constructor
+const obj = new Object();
+
+// add properties with specific values
+obj.property1 = "value1";
+obj.property2 = "value2";
+obj.property3 = "value3";
